196306afa189bb51877c9868c8e48c92db006a67
Kivanio Barbosa <kivanio@gmail.com>
2008-06-04 15:16:06 -0400

    1 - limpando imagens nao usadas 2 - corrigido bug ao copiar imagens

--- eiq_scaffold_generator.rb
+++ eiq_scaffold_generator.rb
@@ -1,18 +1,16 @@
 class EiqScaffoldGenerator < Rails::Generator::NamedBase
 
-  IMAGES = %w[button_add button_back button_cancel button_delete  button_edit  button_help  button_key  button_msg_alert  button_msg_critical  button_msg_info  button_music  button_no  button_ok  button_ok2  button_photo  button_restrito  button_save  button_video  button_view  button_view2  hover  pass  user]
-            
   default_options :skip_timestamps => false, :skip_migration => false
 
   attr_reader   :controller_name,
-                :controller_class_path,
-                :controller_file_path,
-                :controller_class_nesting,
-                :controller_class_nesting_depth,
-                :controller_class_name,
-                :controller_underscore_name,
-                :controller_singular_name,
-                :controller_plural_name
+  :controller_class_path,
+  :controller_file_path,
+  :controller_class_nesting,
+  :controller_class_nesting_depth,
+  :controller_class_name,
+  :controller_underscore_name,
+  :controller_singular_name,
+  :controller_plural_name
   alias_method  :controller_file_name,  :controller_underscore_name
   alias_method  :controller_table_name, :controller_plural_name
 
@@ -48,32 +46,51 @@ class EiqScaffoldGenerator < Rails::Generator::NamedBase
 
       for action in scaffold_views
         m.template(
-          "view_#{action}.html.erb",
-          File.join('app/views', controller_class_path, controller_file_name, "#{action}.html.erb")
+        "view_#{action}.html.erb",
+        File.join('app/views', controller_class_path, controller_file_name, "#{action}.html.erb")
         )
       end
 
       # Layout and stylesheet.
       m.template('_form.html.erb', File.join('app/views', controller_class_path, controller_file_name, "_form.html.erb"))
-      
+
       # Layout admin e partials do admin
       m.template('admin.html.erb', 'app/views/layouts/admin.html.erb')
       m.template('_admin_head.html.erb', 'app/views/layouts/_admin_head.html.erb')
       m.template('_admin_menu.html.erb', 'app/views/layouts/_admin_menu.html.erb')
       m.template('_admin_rodape.html.erb', 'app/views/layouts/_admin_rodape.html.erb')
-      
+
       # CSS do admin
       m.template('admin.css', 'public/stylesheets/admin.css')
-      
-      # Images do CSS
-      for image in EiqScaffoldGenerator::IMAGES
-        m.template("images/#{image}.gif", "public/images/#{image}.gif")
-      end
+
+      # Imagens do CSS
+      m.template('images/button_add.gif', 'public/images/button_add.gif')
+      m.template('images/button_back.gif', 'public/images/button_back.gif')
+      m.template('images/button_cancel.gif', 'public/images/button_cancel.gif')
+      m.template('images/button_delete.gif', 'public/images/button_delete.gif')
+      m.template('images/button_edit.gif', 'public/images/button_edit.gif')
+      m.template('images/button_help.gif', 'public/images/button_help.gif')
+      m.template('images/button_key.gif', 'public/images/button_key.gif')
+      m.template('images/button_msg_alert.gif', 'public/images/button_msg_alert.gif')
+      m.template('images/button_music.gif', 'public/images/button_music.gif')
+      m.template('images/button_no.gif', 'public/images/button_no.gif')
+      m.template('images/button_ok.gif', 'public/images/button_ok.gif')
+      m.template('images/button_ok2.gif', 'public/images/button_ok2.gif')
+      m.template('images/button_photo.gif', 'public/images/button_photo.gif')
+      m.template('images/button_restrito.gif', 'public/images/button_restrito.gif')
+      m.template('images/button_save.gif', 'public/images/button_save.gif')
+      m.template('images/button_video.gif', 'public/images/button_video.gif')
+      m.template('images/button_view.gif', 'public/images/button_view.gif')
+      m.template('images/button_view2.gif', 'public/images/button_view2.gif')
+      m.template('images/hover.gif', 'public/images/hover.gif')
+      m.template('images/pass.gif', 'public/images/pass.gif')
+      m.template('images/user.gif', 'public/images/user.gif')
+      # fim imagens
 
       m.dependency 'model', [name] + @args, :collision => :skip
 
       m.template(
-        'controller.rb', File.join('app/controllers', controller_class_path, "#{controller_file_name}_controller.rb")
+      'controller.rb', File.join('app/controllers', controller_class_path, "#{controller_file_name}_controller.rb")
       )
 
       m.template('functional_test.rb', File.join('test/functional', controller_class_path, "#{controller_file_name}_controller_test.rb"))
@@ -84,25 +101,25 @@ class EiqScaffoldGenerator < Rails::Generator::NamedBase
   end
 
   protected
-    # Override with your own usage banner.
-    def banner
-      "Usando: #{$0} eiq_scaffold NomeModel [campo:tipo, campo:tipo]"
-    end
+  # Override with your own usage banner.
+  def banner
+    "Usando: #{$0} eiq_scaffold NomeModel [campo:tipo, campo:tipo]"
+  end
 
-    def add_options!(opt)
-      opt.separator ''
-      opt.separator 'Opcoes:'
-      opt.on("--skip-timestamps",
-             "Nao adicione timestamps na migration para este model") { |v| options[:skip_timestamps] = v }
-      opt.on("--skip-migration",
-             "Nao criar a migration para este model") { |v| options[:skip_migration] = v }
-    end
+  def add_options!(opt)
+    opt.separator ''
+    opt.separator 'Opcoes:'
+    opt.on("--skip-timestamps",
+    "Nao adicione timestamps na migration para este model") { |v| options[:skip_timestamps] = v }
+    opt.on("--skip-migration",
+    "Nao criar a migration para este model") { |v| options[:skip_migration] = v }
+  end
 
-    def scaffold_views
-      %w[ index show new edit ]
-    end
+  def scaffold_views
+    %w[ index show new edit ]
+  end
 
-    def model_name
-      class_name.demodulize
-    end
+  def model_name
+    class_name.demodulize
+  end
 end
